9bbb85a6577c3231e26ae20cf8f67b8d99f8dde2
CodeMonkeyJeffGT <18846938586@163.com>
2018-07-09 11:05:46 +0800

    debug

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Controller\BaseController as Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Nefu\Nefuer;
 use Wechat\Wechat;
 
