d68828777a376555c4c4c8a16f4c0cea0e511c07
CodeMonkeyJeffGT <18846938586@163.com>
2018-07-13 02:53:35 +0800

    debug

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -19,7 +19,7 @@ class UserController extends Controller
      */
     public function index(WechatService $wechatService): JsonResponse
     {
-        if ($this->session->has('openid')) {
+        if ($this->session->has('nefuer_openid')) {
             return $this->signByOpenid($this->request, $this->session);
         } elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger') !== false) {
             return $this->wxIn($wechatService);
